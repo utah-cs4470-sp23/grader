eb5bb1ba65985a97f9d5bf7d4a4a00843ed8d97f
Pavel Panchekha <me@pavpanchekha.com>
2023-02-09 09:44:54 -0700

    Rename hw6/ok/ to use fixed-width names

--- split.sh
+++ split.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 i=1
 cat "$1" | while read l; do
-    printf "%s\n" "$l" > "$2"/"$i".jpl
+    printf -v fname "%03d" "$i"
+    printf "%s\n" "$l" > "$2"/"$fname".jpl
     i=$((i + 1))
 done
