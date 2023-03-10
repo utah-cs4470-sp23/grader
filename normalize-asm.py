c2c27470e091534b507c414106a1a7198beeffc8
Pavel Panchekha <me@pavpanchekha.com>
2023-03-10 11:44:11 -0700

    Fix math

--- normalize-asm.py
+++ normalize-asm.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 import re
-MATH_RE = re.compile(r"(\d)+\s*([-+])\s*(\d)+")
+MATH_RE = re.compile(r"([-+])\s*(\d)+\s*([-+])\s*(\d)+")
 
 def domath(m):
-    lhs, op, rhs = m.group(1, 2, 3)
-    if op == "+":
-        return str(int(lhs) + int(rhs))
-    elif op == "-":
-        return str(int(lhs) - int(rhs))
-    else:
-        raise ValueError("Invalid operand " + op)
+    op1, lhs, op2, rhs = m.group(1, 2, 3, 4)
+    v1 = -int(lhs) if op1 == "-" else int(lhs)
+    v2 = -int(rhs) if op2 == "-" else int(rhs)
+    v = v1 + v2
+    if v < 0:
+        return "- " + str(abs(v))
+    elif v == 0:
+        return ""
+    elif v > 0:
+        return "- " + str(abs(v))
 
 def normalize(line):
     if ";" in line:
         line = line.split(";", 1)[0]
     line = MATH_RE.sub(domath, line)
     line = line.strip()
+    line = " ".join(line.split())
     return line
 
 if __name__ == "__main__":
